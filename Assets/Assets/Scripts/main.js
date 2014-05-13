ac6f293be01828752415de1b72d627becde0c1e6
Sonny Lin <jagdgeschwader262000@gmail.com>
2014-05-13 16:35:47 -0700

    Initial Commit

---
+++ Assets/Assets/Scripts/main.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var node:GameObject;
+
+function Start () {
+	for(var i:int = 0; i < 5; i++){
+		Instantiate(node,  
+		Vector3(Random.value*10 + this.camera.transform.position.x, 
+		0, 
+		Random.value*10 + this.camera.transform.position.z), Quaternion.identity);
+	}
+}
+
+function Update () {
+
+}
